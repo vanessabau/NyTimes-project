a86601dace7e9564fdb29a84da24c1051f25640e
Vanessa Bautista <vbautista916@icloud.com>
2020-05-02 14:17:00 -0700

    fixed buttons and query links

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,15 @@
-
-$("#search-button").on("click", function(event) {
-    event.preventDefault();
-    clearInterval();
-    console.log(this);
+$(document).ready(function(){
+$("#search-button").on("click", function() {
+    console.log("hey");
+    //event.preventDefault();
+   
     //NYT Api query
     
     var startYear = "2010";
     var endYear = "2020";
     var subject = "obama";
     var sourcesNum = 5;
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=whR9vKaMMAqHi8Y14STehdAAgj5duFIn" 
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=obama&facet_fields=source&facet=true&begin_date=20120101&end_date=20121231&api-key=8lDbBlloTPEqY4nZFBdkrGq6t6PMN7vu";
     
     //Load data using get request with ajax format
     $.ajax({
@@ -18,7 +18,9 @@ $("#search-button").on("click", function(event) {
     })
     //after loading data response, do the following
       .then(function(response) {
+        console.log(response);
       });
   });
-
+});
+console.log("hello");
   
